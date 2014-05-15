b03944417640535ae938fe55ae9d7cde44759016
Neil Bowers <neil@bowers.com>
2014-05-15 22:18:17 +0100

    Added a SEE ALSO section to the doc

--- lib/again.pm
+++ lib/again.pm
@@ -120,6 +120,19 @@ function that you first loaded.
 
 =back
 
+=head1 SEE ALSO
+
+L<Module::Reload> provides a class method which checks all
+loaded modules to see if the file on disk has changed since the module
+was loaded. It was last updated in 1998, and I haven't tested it.
+
+L<Class::Unload> unloads a class, by clearing out its symbol table
+and removing it from C<%INC>.
+
+L<Padre::Unload> is part of the L<Padre> IDE. It's similar to Class::Unload,
+but says it has "a few more tricks up its sleeve". It's not documented though,
+so just intended for internal use in Padre.
+
 =head1 REPOSITORY
 
 L<https://github.com/neilbowers/again>
